bd991221bd1b21fef5fa06936da2dc79397c9321
Thales de Mattos <thales_de_mattos@hotmail.com>
2018-03-16 02:15:30 -0300

    Início da dinâmica de jogadas

--- PR1A5/src/pr1a5/A05Damas.java
+++ PR1A5/src/pr1a5/A05Damas.java
@@ -6,16 +6,15 @@
 package pr1a5;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  *
  * @author Thales
  */
-
-
 public class A05Damas {
-    
-    public static void mostraTabuleiro(String[][] tabuleiro){ //Mostra o tabuleiro preenchido na tela.
+
+    public static void MostraTabuleiro(String[][] tabuleiro) { //Mostra o tabuleiro preenchido na tela.
         System.out.println("   --- --- --- --- --- --- --- ---");
         System.out.printf("1 | %s | %s | %s | %s | %s | %s | %s | %s |\n", tabuleiro[0][0], tabuleiro[0][1], tabuleiro[0][2], tabuleiro[0][3], tabuleiro[0][4], tabuleiro[0][5], tabuleiro[0][6], tabuleiro[0][7]);
         System.out.println("   --- --- --- --- --- --- --- ---");
@@ -35,30 +34,33 @@ public class A05Damas {
         System.out.println("   --- --- --- --- --- --- --- ---");
         System.out.println("    1   2   3   4   5   6   7   8");
     }
-    
-    public static String[][] inicializaTabuleiro(){
+
+    public static String[][] InicializaTabuleiro() {
         String[][] tabuleiro = new String[8][8];
-        
-        for(int linha = 0; linha < 8; linha++){
-            for(int coluna = 0; coluna < 8; coluna++){
-                switch(linha){
+
+        for (int linha = 0; linha < 8; linha++) {
+            for (int coluna = 0; coluna < 8; coluna++) {
+                switch (linha) {
                     case 0:
-                        if(coluna % 2 == 1)
-                            tabuleiro[linha][coluna] = "B";
-                        else
+                        if (coluna % 2 == 1) {
+                            tabuleiro[linha][coluna] = "b";
+                        } else {
                             tabuleiro[linha][coluna] = " ";
+                        }
                         break;
                     case 1:
-                        if(coluna % 2 == 0)
-                            tabuleiro[linha][coluna] = "B";
-                        else
+                        if (coluna % 2 == 0) {
+                            tabuleiro[linha][coluna] = "b";
+                        } else {
                             tabuleiro[linha][coluna] = " ";
+                        }
                         break;
                     case 2:
-                        if(coluna % 2 == 1)
-                            tabuleiro[linha][coluna] = "B";
-                        else
+                        if (coluna % 2 == 1) {
+                            tabuleiro[linha][coluna] = "b";
+                        } else {
                             tabuleiro[linha][coluna] = " ";
+                        }
                         break;
                     case 3:
                         tabuleiro[linha][coluna] = " ";
@@ -67,22 +69,25 @@ public class A05Damas {
                         tabuleiro[linha][coluna] = " ";
                         break;
                     case 5:
-                        if(coluna % 2 == 0)
-                            tabuleiro[linha][coluna] = "W";
-                        else
+                        if (coluna % 2 == 0) {
+                            tabuleiro[linha][coluna] = "w";
+                        } else {
                             tabuleiro[linha][coluna] = " ";
+                        }
                         break;
                     case 6:
-                        if(coluna % 2 == 1)
-                            tabuleiro[linha][coluna] = "W";
-                        else
+                        if (coluna % 2 == 1) {
+                            tabuleiro[linha][coluna] = "w";
+                        } else {
                             tabuleiro[linha][coluna] = " ";
+                        }
                         break;
                     case 7:
-                        if(coluna % 2 == 0)
-                            tabuleiro[linha][coluna] = "W";
-                        else
+                        if (coluna % 2 == 0) {
+                            tabuleiro[linha][coluna] = "w";
+                        } else {
                             tabuleiro[linha][coluna] = " ";
+                        }
                         break;
                 }
             }
@@ -90,8 +95,32 @@ public class A05Damas {
         return tabuleiro;
     }
     
+    public static int Vitoria(){
+        return 0;
+    }
+
     public static void main(String[] args) {
-        String[][] tabuleiro = inicializaTabuleiro();
-        mostraTabuleiro(tabuleiro);
+        //TODO: Imprimir Regras, Validar Jogadas, Realizar Jogadas, Alternar Vez, Definir Vitória, Permitir Empate
+        boolean vezBranco = true; //Variável de controle de vez
+        int linhaPeca, colunaPeca; //Variáveis de entrada de seleção de peça para a jogada
+        int linhaAlvo, colunaAlvo; //Variáveis de entrada de seleção de espaço a ser ocupado pela peça selecionada
+        Scanner scan = new Scanner(System.in); //Scanner para leitura de input
+        String[][] tabuleiro = InicializaTabuleiro(); //Preenche o tabuleiro com as posições iniciais
+        while (Vitoria() == 0) { //Mantém o laço até que um jogar seja vitorioso
+            MostraTabuleiro(tabuleiro); //Função que imprime o tabuleiro atual na tela
+            System.out.println("\n\n");
+            if(vezBranco) //Verifica e imprime de quem é a vez
+                System.out.println("Vez das peças brancas!");
+            else
+                System.out.println("Vez das peças pretas!");
+            do{ //Laço de pós-teste para validação de dados                
+                System.out.println("Insira a linha da peça que deseja mover: ");
+                linhaPeca = scan.nextInt();
+                System.out.println("Insira a coluna da peça que deseja mover: ");
+                colunaPeca = scan.nextInt();
+                if((vezBranco && !tabuleiro[linhaPeca][colunaPeca].equals("W") && !tabuleiro[linhaPeca][colunaPeca].equals("w")) || (!vezBranco && !tabuleiro[linhaPeca][colunaPeca].equals("B") && !tabuleiro[linhaPeca][colunaPeca].equals("w")) || (!vezBranco && !tabuleiro[linhaPeca][colunaPeca].equals("b"))) //Verifica se a peça selecionada é válida para o jogador da vez
+                    System.out.println("Essa posição na corresponde a uma peça sua. Por favor jogue novamente.\n\n\n");                
+            }while((vezBranco && !tabuleiro[linhaPeca][colunaPeca].equals("W") && !tabuleiro[linhaPeca][colunaPeca].equals("w")) || (!vezBranco && !tabuleiro[linhaPeca][colunaPeca].equals("B") && !tabuleiro[linhaPeca][colunaPeca].equals("w")) || (!vezBranco && !tabuleiro[linhaPeca][colunaPeca].equals("b")));            
+        }
     }
 }
