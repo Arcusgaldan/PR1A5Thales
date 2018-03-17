a4ae0bf59cd14a9d77cfba37729d4f53a12124ad
Thales de Mattos <thales_de_mattos@hotmail.com>
2018-03-17 01:10:36 -0300

    Refatoração de String para char na matriz "tabuleiro".

--- PR1A5/src/pr1a5/A05Damas.java
+++ PR1A5/src/pr1a5/A05Damas.java
@@ -12,81 +12,94 @@ import java.util.Scanner;
  *
  * @author Thales
  */
+
+class Posicao{
+    int linha;
+    int coluna;
+    char tipo;
+    
+    public Posicao(int linha, int coluna, char tipo){
+        this.linha = linha;
+        this.coluna = coluna;
+        this.tipo = tipo;
+    }
+}
+
 public class A05Damas {
 
-    public static void MostraTabuleiro(String[][] tabuleiro) { //Mostra o tabuleiro preenchido na tela.
+    public static void MostraTabuleiro(char[][] tabuleiro) { //Mostra o tabuleiro preenchido na tela.
         System.out.println("   --- --- --- --- --- --- --- ---");
-        System.out.printf("1 | %s | %s | %s | %s | %s | %s | %s | %s |\n", tabuleiro[0][0], tabuleiro[0][1], tabuleiro[0][2], tabuleiro[0][3], tabuleiro[0][4], tabuleiro[0][5], tabuleiro[0][6], tabuleiro[0][7]);
+        System.out.printf("1 | %c | %c | %c | %c | %c | %c | %c | %c |\n", tabuleiro[0][0], tabuleiro[0][1], tabuleiro[0][2], tabuleiro[0][3], tabuleiro[0][4], tabuleiro[0][5], tabuleiro[0][6], tabuleiro[0][7]);
         System.out.println("   --- --- --- --- --- --- --- ---");
-        System.out.printf("2 | %s | %s | %s | %s | %s | %s | %s | %s |\n", tabuleiro[1][0], tabuleiro[1][1], tabuleiro[1][2], tabuleiro[1][3], tabuleiro[1][4], tabuleiro[1][5], tabuleiro[1][6], tabuleiro[1][7]);
+        System.out.printf("2 | %c | %c | %c | %c | %c | %c | %c | %c |\n", tabuleiro[1][0], tabuleiro[1][1], tabuleiro[1][2], tabuleiro[1][3], tabuleiro[1][4], tabuleiro[1][5], tabuleiro[1][6], tabuleiro[1][7]);
         System.out.println("   --- --- --- --- --- --- --- ---");
-        System.out.printf("3 | %s | %s | %s | %s | %s | %s | %s | %s |\n", tabuleiro[2][0], tabuleiro[2][1], tabuleiro[2][2], tabuleiro[2][3], tabuleiro[2][4], tabuleiro[2][5], tabuleiro[2][6], tabuleiro[2][7]);
+        System.out.printf("3 | %c | %c | %c | %c | %c | %c | %c | %c |\n", tabuleiro[2][0], tabuleiro[2][1], tabuleiro[2][2], tabuleiro[2][3], tabuleiro[2][4], tabuleiro[2][5], tabuleiro[2][6], tabuleiro[2][7]);
         System.out.println("   --- --- --- --- --- --- --- ---");
-        System.out.printf("4 | %s | %s | %s | %s | %s | %s | %s | %s |\n", tabuleiro[3][0], tabuleiro[3][1], tabuleiro[3][2], tabuleiro[3][3], tabuleiro[3][4], tabuleiro[3][5], tabuleiro[3][6], tabuleiro[3][7]);
+        System.out.printf("4 | %c | %c | %c | %c | %c | %c | %c | %c |\n", tabuleiro[3][0], tabuleiro[3][1], tabuleiro[3][2], tabuleiro[3][3], tabuleiro[3][4], tabuleiro[3][5], tabuleiro[3][6], tabuleiro[3][7]);
         System.out.println("   --- --- --- --- --- --- --- ---");
-        System.out.printf("5 | %s | %s | %s | %s | %s | %s | %s | %s |\n", tabuleiro[4][0], tabuleiro[4][1], tabuleiro[4][2], tabuleiro[4][3], tabuleiro[4][4], tabuleiro[4][5], tabuleiro[4][6], tabuleiro[4][7]);
+        System.out.printf("5 | %c | %c | %c | %c | %c | %c | %c | %c |\n", tabuleiro[4][0], tabuleiro[4][1], tabuleiro[4][2], tabuleiro[4][3], tabuleiro[4][4], tabuleiro[4][5], tabuleiro[4][6], tabuleiro[4][7]);
         System.out.println("   --- --- --- --- --- --- --- ---");
-        System.out.printf("6 | %s | %s | %s | %s | %s | %s | %s | %s |\n", tabuleiro[5][0], tabuleiro[5][1], tabuleiro[5][2], tabuleiro[5][3], tabuleiro[5][4], tabuleiro[5][5], tabuleiro[5][6], tabuleiro[5][7]);
+        System.out.printf("6 | %c | %c | %c | %c | %c | %c | %c | %c |\n", tabuleiro[5][0], tabuleiro[5][1], tabuleiro[5][2], tabuleiro[5][3], tabuleiro[5][4], tabuleiro[5][5], tabuleiro[5][6], tabuleiro[5][7]);
         System.out.println("   --- --- --- --- --- --- --- ---");
-        System.out.printf("7 | %s | %s | %s | %s | %s | %s | %s | %s |\n", tabuleiro[6][0], tabuleiro[6][1], tabuleiro[6][2], tabuleiro[6][3], tabuleiro[6][4], tabuleiro[6][5], tabuleiro[6][6], tabuleiro[6][7]);
+        System.out.printf("7 | %c | %c | %c | %c | %c | %c | %c | %c |\n", tabuleiro[6][0], tabuleiro[6][1], tabuleiro[6][2], tabuleiro[6][3], tabuleiro[6][4], tabuleiro[6][5], tabuleiro[6][6], tabuleiro[6][7]);
         System.out.println("   --- --- --- --- --- --- --- ---");
-        System.out.printf("8 | %s | %s | %s | %s | %s | %s | %s | %s |\n", tabuleiro[7][0], tabuleiro[7][1], tabuleiro[7][2], tabuleiro[7][3], tabuleiro[7][4], tabuleiro[7][5], tabuleiro[7][6], tabuleiro[7][7]);
+        System.out.printf("8 | %c | %c | %c | %c | %c | %c | %c | %c |\n", tabuleiro[7][0], tabuleiro[7][1], tabuleiro[7][2], tabuleiro[7][3], tabuleiro[7][4], tabuleiro[7][5], tabuleiro[7][6], tabuleiro[7][7]);
         System.out.println("   --- --- --- --- --- --- --- ---");
         System.out.println("    1   2   3   4   5   6   7   8");
     }
 
-    public static String[][] InicializaTabuleiro() {
-        String[][] tabuleiro = new String[8][8];
+    public static char[][] InicializaTabuleiro() { //Preenche o tabuleiro com as posições iniciais.
+        char[][] tabuleiro = new char[8][8];
 
         for (int linha = 0; linha < 8; linha++) {
             for (int coluna = 0; coluna < 8; coluna++) {
                 switch (linha) {
                     case 0:
                         if (coluna % 2 == 1) {
-                            tabuleiro[linha][coluna] = "b";
+                            tabuleiro[linha][coluna] = 'b';
                         } else {
-                            tabuleiro[linha][coluna] = " ";
+                            tabuleiro[linha][coluna] = ' ';
                         }
                         break;
                     case 1:
                         if (coluna % 2 == 0) {
-                            tabuleiro[linha][coluna] = "b";
+                            tabuleiro[linha][coluna] = 'b';
                         } else {
-                            tabuleiro[linha][coluna] = " ";
+                            tabuleiro[linha][coluna] = ' ';
                         }
                         break;
                     case 2:
                         if (coluna % 2 == 1) {
-                            tabuleiro[linha][coluna] = "b";
+                            tabuleiro[linha][coluna] = 'b';
                         } else {
-                            tabuleiro[linha][coluna] = " ";
+                            tabuleiro[linha][coluna] = ' ';
                         }
                         break;
                     case 3:
-                        tabuleiro[linha][coluna] = " ";
+                        tabuleiro[linha][coluna] = ' ';
                         break;
                     case 4:
-                        tabuleiro[linha][coluna] = " ";
+                        tabuleiro[linha][coluna] = ' ';
                         break;
                     case 5:
                         if (coluna % 2 == 0) {
-                            tabuleiro[linha][coluna] = "w";
+                            tabuleiro[linha][coluna] = 'w';
                         } else {
-                            tabuleiro[linha][coluna] = " ";
+                            tabuleiro[linha][coluna] = ' ';
                         }
                         break;
                     case 6:
                         if (coluna % 2 == 1) {
-                            tabuleiro[linha][coluna] = "w";
+                            tabuleiro[linha][coluna] = 'w';
                         } else {
-                            tabuleiro[linha][coluna] = " ";
+                            tabuleiro[linha][coluna] = ' ';
                         }
                         break;
                     case 7:
                         if (coluna % 2 == 0) {
-                            tabuleiro[linha][coluna] = "w";
+                            tabuleiro[linha][coluna] = 'w';
                         } else {
-                            tabuleiro[linha][coluna] = " ";
+                            tabuleiro[linha][coluna] = ' ';
                         }
                         break;
                 }
@@ -98,6 +111,11 @@ public class A05Damas {
     public static int Vitoria(){
         return 0;
     }
+    
+    public static boolean ValidaJogada(String[][] tabuleiro, Posicao peca, Posicao alvo, boolean vezBranco){
+        
+        return true;
+    }
 
     public static void main(String[] args) {
         //TODO: Imprimir Regras, Validar Jogadas, Realizar Jogadas, Alternar Vez, Definir Vitória, Permitir Empate
@@ -105,7 +123,7 @@ public class A05Damas {
         int linhaPeca, colunaPeca; //Variáveis de entrada de seleção de peça para a jogada
         int linhaAlvo, colunaAlvo; //Variáveis de entrada de seleção de espaço a ser ocupado pela peça selecionada
         Scanner scan = new Scanner(System.in); //Scanner para leitura de input
-        String[][] tabuleiro = InicializaTabuleiro(); //Preenche o tabuleiro com as posições iniciais
+        char[][] tabuleiro = InicializaTabuleiro(); //Preenche o tabuleiro com as posições iniciais
         while (Vitoria() == 0) { //Mantém o laço até que um jogar seja vitorioso
             MostraTabuleiro(tabuleiro); //Função que imprime o tabuleiro atual na tela
             System.out.println("\n\n");
@@ -113,14 +131,25 @@ public class A05Damas {
                 System.out.println("Vez das peças brancas!");
             else
                 System.out.println("Vez das peças pretas!");
-            do{ //Laço de pós-teste para validação de dados                
-                System.out.println("Insira a linha da peça que deseja mover: ");
+            do{ //Laço de pós-teste para validação de escolha de peça              
+                System.out.print("Insira a linha da peça que deseja mover: ");
                 linhaPeca = scan.nextInt();
-                System.out.println("Insira a coluna da peça que deseja mover: ");
+                linhaPeca--;
+                System.out.print("Insira a coluna da peça que deseja mover: ");
                 colunaPeca = scan.nextInt();
-                if((vezBranco && !tabuleiro[linhaPeca][colunaPeca].equals("W") && !tabuleiro[linhaPeca][colunaPeca].equals("w")) || (!vezBranco && !tabuleiro[linhaPeca][colunaPeca].equals("B") && !tabuleiro[linhaPeca][colunaPeca].equals("w")) || (!vezBranco && !tabuleiro[linhaPeca][colunaPeca].equals("b"))) //Verifica se a peça selecionada é válida para o jogador da vez
+                colunaPeca--;
+                if((vezBranco && tabuleiro[linhaPeca][colunaPeca] != 'W' && tabuleiro[linhaPeca][colunaPeca] != 'w') || (!vezBranco && tabuleiro[linhaPeca][colunaPeca] != 'B' && tabuleiro[linhaPeca][colunaPeca] != 'b')) //Verifica se a peça selecionada é válida para o jogador da vez
                     System.out.println("Essa posição na corresponde a uma peça sua. Por favor jogue novamente.\n\n\n");                
-            }while((vezBranco && !tabuleiro[linhaPeca][colunaPeca].equals("W") && !tabuleiro[linhaPeca][colunaPeca].equals("w")) || (!vezBranco && !tabuleiro[linhaPeca][colunaPeca].equals("B") && !tabuleiro[linhaPeca][colunaPeca].equals("w")) || (!vezBranco && !tabuleiro[linhaPeca][colunaPeca].equals("b")));            
+            }while((vezBranco && tabuleiro[linhaPeca][colunaPeca] != 'W' && tabuleiro[linhaPeca][colunaPeca] != 'w') || (!vezBranco && tabuleiro[linhaPeca][colunaPeca] != 'B' && tabuleiro[linhaPeca][colunaPeca] != 'b'));            
+            Posicao peca = new Posicao(linhaPeca, colunaPeca, tabuleiro[linhaPeca][colunaPeca]);
+            do{
+                System.out.print("Insira a linha da casa que deseja ocupar: ");
+                linhaAlvo = scan.nextInt();
+                linhaAlvo--;
+                System.out.print("Insira a coluna da casa que deseja ocupar: ");
+                colunaAlvo = scan.nextInt();
+                colunaAlvo--;
+            }while(true);
         }
     }
 }
